4d4b7ec1cdc695b3a1cd36b6be1d20a1bf64cd11
Dani <daniellopez.macia@gmail.com>
2022-11-16 08:07:21 +0100

    all endpoints done

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const db = new sqlite3.Database(path.resolve('db.sqlite'), sqlite3.OPEN_READWRIT
     if (err) return console.error(err.message);
 })
 // for the sql statements
-let sql
+let sql;
+const saltRounds = 10;
 
 const app = express();
 app.use(bp.json());
@@ -19,48 +20,123 @@ app.use(bp.urlencoded({ extended: true}));
 
 // get all users
 app.get('/', (req, res) => {
-    console.log(`GET ALL`)
+    try{
 
-    sql = 'SELECT id, username FROM users';
-    db.all(sql, [], (err, rows) => {
-        if (err) return res.json({ status: 300, success: false, error: err})
-        return res.status(200).json(rows);
-    })
+        sql = 'SELECT id, username FROM users';
+        db.all(sql, [], (err, rows) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            return res.status(200).json(rows);
+        })
+
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
 })
 
 // get user by ID
 app.get('/:user_id', (req, res) => {
-    console.log(`GET ${req.params.user_id}`)
+    try{
 
-    sql = 'SELECT id, username FROM users WHERE id=?';
-    db.all(sql, [req.params.user_id], (err, rows) => {
-        if (err) return res.json({ status: 300, success: false, error: err})
-        return res.status(200).json(rows);
-    })
+        sql = 'SELECT id, username FROM users WHERE id=?';
+        db.get(sql, [req.params.user_id], (err, rows) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            return res.status(200).json(rows);
+        })
+        
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
 })
 
 // register a user unless USERNAME already in use
 app.post('/signup', (req, res) => {
-    const {username, password} = req.body;
-    sql = 'SELECT id FROM users WHERE username = ?';
-    db.all(sql, username, (err, rows) => {
-        if (err) return res.json({ status: 300, success: false, error: err})
+    try {
+            
+        const {username, password} = req.body;
+        sql = 'SELECT id FROM users WHERE username = ?';
+        db.get(sql, username, (err, row) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            if (row) return res.json({ status: 300, success: false, error: "Username already exists"})
 
-        if (rows.length) return res.json({ status: 300, success: false, error: "Username already exists"})
-
-        const saltRounds = 10
-        bcrypt.genSalt(saltRounds, function (err, salt) {
-            bcrypt.hash(password, salt, function (err, hash) {
-                if (err) return res.json({ status: 300, success: false, error: err})
-
-                sql = 'INSERT INTO users(username,password) VALUES (?,?)'
-                db.run(sql, [username, hash], (err) => {
+            
+            bcrypt.genSalt(saltRounds, function (err, salt) {
+                bcrypt.hash(password, salt, function (err, hash) {
                     if (err) return res.json({ status: 300, success: false, error: err})
+
+                    sql = 'INSERT INTO users(username,password) VALUES (?,?)'
+                    db.run(sql, [username, hash], (err) => {
+                        if (err) return res.json({ status: 300, success: false, error: err})
+
+                        
+                        return res.json({ status: 200, success: true})
+                    })
                 })
-                return res.json({ status: 200, success: true})
             })
         })
-    })
+
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
+})
+
+// log in a user
+app.post('/login', (req, res) => {
+    try {
+            
+        const {username, password} = req.body;
+        sql = 'SELECT password FROM users WHERE username = ?';
+        db.get(sql, username, (err, row) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            if (!row) return res.json({ status: 400, success: false, error: "user not found"})
+            bcrypt.compare(password, row.password, function (err, response) {
+                if (err) return res.json({ status: 300, success: false, error: err});
+                
+                if(!response){
+
+                    return res.json({ status: 400, success: false});
+                }else{
+                        
+                    //req.session.user = username;
+                    //req.session.userid = success;
+                    return res.json({ status: 200, success: true});
+                }
+            })
+        })
+        
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
+})
+
+// change a user's USERNAME given it's ID
+app.put('/:user_id', (req, res) => {
+    try {
+        
+        const {username} = req.body;
+        sql = 'UPDATE users SET username = ? WHERE id = ?';
+        db.run(sql, [username, req.params.user_id], (err) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            return res.json({ status: 200, success: true})
+        })
+        
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
+})
+
+// delete a user given it's ID
+app.delete('/:user_id', (req, res) => {
+    try{
+
+        sql = 'DELETE FROM users WHERE id = ?';
+        db.run(sql, [req.params.user_id], (err) => {
+            if (err) return res.json({ status: 300, success: false, error: err})
+            return res.json({ status: 200, success: true})
+        })
+        
+    } catch (error) {
+        return res.json({ status: 400, success: false})
+    }
 })
 
 app.listen(1234, () => {
